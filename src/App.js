6b0ace156e350588a179a5e96840cfd1d54c4c4f
Hasmukh Rathod <guru__@hotmail.com>
2020-03-05 16:12:29 +0100

    props

--- src/App.js
+++ src/App.js
@@ -35,9 +35,16 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
